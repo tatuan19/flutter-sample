3b5dc26fa4561c95a4e92910121c50ae1ff285e8
tuan.tran <tuan.tran@linkbal.co.jp>
2023-12-19 11:13:17 +0900

    Display login dialog

--- lib/ui/store/store_screen.dart
+++ lib/ui/store/store_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:hive/hive.dart';
 import 'package:sample/common/constants/local_data_keys.dart';
+import 'package:sample/common/themes/sizes.dart';
 import 'package:sample/common/widgets/custom_app_bar.dart';
 import 'package:sample/ui/web_view/custom_web_view.dart';
 
@@ -20,24 +21,115 @@ class StoreScreen extends StatelessWidget {
 
     return Scaffold(
       appBar: const CustomAppBar(title: Text('店舗で参加')),
-      body: Stack(
-        children: [
-          CustomWebView(
-              initialUrl: url,
-              onLoadStop: () async {
-                cookieManager
-                    .getCookie(url: Uri.parse(url), name: "_couplink_sess")
-                    .then((cookies) {
-                  final sessionId = cookies?.value;
+      body: CustomWebView(
+        initialUrl: url,
+        onLoadStop: () async {
+          cookieManager
+              .getCookie(url: Uri.parse(url), name: "_couplink_sess")
+              .then((cookies) {
+            final sessionId = cookies?.value;
 
-                  if (sessionId != null && sessionId.isNotEmpty) {
-                    devtools.log('sessionId: $sessionId');
-                    Hive.box(localDataBox).put(sessionIdKey, sessionId);
-                  }
-                }).catchError((error) {
-                  devtools.log('error: $error');
-                });
-              })
+            if (sessionId != null && sessionId.isNotEmpty) {
+              devtools.log('sessionId: $sessionId');
+              Hive.box(localDataBox).put(sessionIdKey, sessionId);
+            }
+          }).catchError((error) {
+            devtools.log('error: $error');
+          });
+        },
+        onTap: () {
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return RequireLoginDialog(
+                onPositivePressed: () {},
+                onNegativePressed: () {},
+                onRedirect: () {},
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
+
+class RequireLoginDialog extends StatelessWidget {
+  const RequireLoginDialog({
+    super.key,
+    required this.onPositivePressed,
+    required this.onNegativePressed,
+    required this.onRedirect,
+  });
+
+  final VoidCallback onPositivePressed;
+  final VoidCallback onNegativePressed;
+  final VoidCallback onRedirect;
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+      elevation: 0,
+      child: contentBox(context),
+    );
+  }
+
+  Widget contentBox(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 20),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          const Text(
+            'ここから先は、\n会員登録が必要です',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 20),
+          const Text(
+            '会員登録は120秒で完了します',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: FontSize.small),
+          ),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: onPositivePressed,
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.grey[800],
+              foregroundColor: Colors.white,
+            ),
+            child: const Text(
+              '会員登録に進む',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+          ),
+          const SizedBox(height: 5),
+          ElevatedButton(
+            onPressed: onNegativePressed,
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.grey[400],
+              foregroundColor: Colors.white,
+            ),
+            child: const Text(
+              'キャンセル',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+          ),
+          const SizedBox(height: 20),
+          GestureDetector(
+            onTap: onRedirect,
+            child: const Text(
+              '会員の方はこちら',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: FontSize.tiny,
+                fontWeight: FontWeight.bold,
+                decoration: TextDecoration.underline,
+              ),
+            ),
+          ),
         ],
       ),
     );
--- lib/ui/web_view/custom_web_view.dart
+++ lib/ui/web_view/custom_web_view.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
@@ -5,27 +7,37 @@ import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'dart:developer' as devtools show log;
 
 class CustomWebView extends HookWidget {
-  const CustomWebView({super.key, required this.initialUrl, this.onLoadStop});
+  const CustomWebView({
+    super.key,
+    required this.initialUrl,
+    this.gesturesEnabled = false,
+    this.onLoadStop,
+    this.onTap,
+  });
 
   final String initialUrl;
+  final bool gesturesEnabled;
   final Function? onLoadStop;
+  final Function? onTap;
 
   @override
   Widget build(BuildContext context) {
     final webViewKey = useMemoized(() => GlobalKey());
     final webViewController = useState<InAppWebViewController?>(null);
-    final options = useMemoized(() => InAppWebViewGroupOptions(
-          crossPlatform: InAppWebViewOptions(
-            useShouldOverrideUrlLoading: true,
-            mediaPlaybackRequiresUserGesture: false,
-          ),
-          android: AndroidInAppWebViewOptions(
-            useHybridComposition: true,
-          ),
-          ios: IOSInAppWebViewOptions(
-            allowsInlineMediaPlayback: true,
-          ),
-        ));
+    final options = useMemoized(
+      () => InAppWebViewGroupOptions(
+        crossPlatform: InAppWebViewOptions(
+          useShouldOverrideUrlLoading: true,
+          mediaPlaybackRequiresUserGesture: false,
+        ),
+        android: AndroidInAppWebViewOptions(
+          useHybridComposition: true,
+        ),
+        ios: IOSInAppWebViewOptions(
+          allowsInlineMediaPlayback: true,
+        ),
+      ),
+    );
     final pullToRefreshController = useMemoized(() => PullToRefreshController(
           options: PullToRefreshOptions(
             color: Colors.green,
@@ -34,6 +46,10 @@ class CustomWebView extends HookWidget {
             webViewController.value?.reload();
           },
         ));
+
+    void disableActions() async {
+      // TODO: Disable clicks on all elements in the WebView content
+    }
 
     return Stack(
       children: [
@@ -70,11 +86,20 @@ class CustomWebView extends HookWidget {
           onLoadStop: (controller, url) async {
             pullToRefreshController.endRefreshing();
 
+            if (!gesturesEnabled) disableActions();
             onLoadStop != null ? onLoadStop!() : null;
           },
           onLoadError: (controller, url, code, message) {
             pullToRefreshController.endRefreshing();
             devtools.log("Error $code: $message");
+          },
+          gestureRecognizers: <Factory<OneSequenceGestureRecognizer>>{
+            Factory<VerticalDragGestureRecognizer>(
+              () => VerticalDragGestureRecognizer()
+                ..onDown = (value) {
+                  onTap != null ? onTap!() : null;
+                },
+            )
           },
         ),
       ],
